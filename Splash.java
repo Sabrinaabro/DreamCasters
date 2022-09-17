56b9606c0e43a31cb66e18e49a2bd95c99200ddc
Sabrina Abro <108029039+Sabrinaabro@users.noreply.github.com>
2022-09-18 01:29:30 +0500

    Add files via upload

--- Splash.java
+++ Splash.java
@@ -1,3 +1,19 @@
-public class Splash {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame {
+    Splash(){
+        setSize(1200,600);
+        setLocation(200,100);
+        setVisible(true);
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/plane.jpeg"));
+        Image i2= i1.getImage().getScaledInstance(1200,600,Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        add(image);
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
