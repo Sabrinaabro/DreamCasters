4b2203092bb8ea5ea69e82d40beb59f2a320dd27
Sabrina Abro <108029039+Sabrinaabro@users.noreply.github.com>
2022-09-21 02:06:49 +0500

    Add files via upload

--- Splash.java
+++ Splash.java
@@ -1,3 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dreamcasters;
+
+/**
+ *
+ * @author ABRO
+ */
 public class Splash {
     
 }
